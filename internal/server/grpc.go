f80fe3e1ba086a8d229218fdfc199b456367338f
mengbin92 <mengbin1992@outlook.com>
2023-08-24 16:34:08 +0800

    feat(使用selector通过白名单实现JWT过滤):

--- internal/server/grpc.go
+++ internal/server/grpc.go
@@ -6,8 +6,11 @@ import (
 	"mengbin92/browser/internal/service"
 
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/selector"
 	"github.com/go-kratos/kratos/v2/transport/grpc"
+	jwtv4 "github.com/golang-jwt/jwt/v4"
 )
 
 // NewGRPCServer new a gRPC server.
@@ -15,9 +18,11 @@ func NewGRPCServer(c *conf.Server, greeter *service.BrowserService, block *servi
 	var opts = []grpc.ServerOption{
 		grpc.Middleware(
 			recovery.Recovery(),
-			// jwt.Server(func(token *jwtv4.Token) (interface{}, error) {
-			// 	return []byte(c.Auth.JwtSecret), nil
-			// }),
+			selector.Server(
+				jwt.Server(func(token *jwtv4.Token) (interface{}, error) {
+				return []byte(c.Auth.JwtSecret), nil
+			},
+			)).Match(whiteMatcher()).Build(),
 		),
 	}
 	if c.Grpc.Network != "" {
--- internal/server/http.go
+++ internal/server/http.go
@@ -1,13 +1,17 @@
 package server
 
 import (
+	"context"
 	v1 "mengbin92/browser/api/browser/v1"
 	"mengbin92/browser/internal/conf"
 	"mengbin92/browser/internal/service"
 
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/auth/jwt"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/middleware/selector"
 	"github.com/go-kratos/kratos/v2/transport/http"
+	jwtv4 "github.com/golang-jwt/jwt/v4"
 )
 
 // NewHTTPServer new an HTTP server.
@@ -15,9 +19,11 @@ func NewHTTPServer(c *conf.Server, browser *service.BrowserService, block *servi
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
-			// jwt.Server(func(token *jwtv4.Token) (interface{}, error) {
-			// 	return []byte(c.Auth.JwtSecret), nil
-			// }),
+			selector.Server(
+				jwt.Server(func(token *jwtv4.Token) (interface{}, error) {
+					return []byte(c.Auth.JwtSecret), nil
+				}),
+			).Match(whiteMatcher()).Build(),
 		),
 	}
 	if c.Http.Network != "" {
@@ -33,4 +39,16 @@ func NewHTTPServer(c *conf.Server, browser *service.BrowserService, block *servi
 	v1.RegisterBrowserHTTPServer(srv, browser)
 	v1.RegisterBlockHTTPServer(srv, block)
 	return srv
+}
+
+func whiteMatcher() selector.MatchFunc {
+	whitelist := make(map[string]struct{})
+	whitelist["/api.browser.v1.Browser/GetToken"] = struct{}{}
+	whitelist["/api.browser.v1.Browser/Regisger"] = struct{}{}
+	return func(ctx context.Context, path string) bool {
+		if _, ok := whitelist[path]; ok {
+			return false
+		}
+		return true
+	}
 }
